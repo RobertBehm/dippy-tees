3630821b11f8db155edf10f83eb1ed798f61bd8b
Robert Behm <matthewrobertbehm@gmail.com>
2021-12-06 16:51:40 +0000

    Updated server config for NGINX

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env("URL", "http://localhost"),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '29018098ba4d2d0b0f3bd12a72d8f37c'),
