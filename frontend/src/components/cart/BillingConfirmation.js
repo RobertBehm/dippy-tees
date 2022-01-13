1904b2e74d1cf2e31c40034fa4131a64261bdbbd
RobertBehm <59755088+RobertBehm@users.noreply.github.com>
2022-01-13 12:49:42 -0500

    changed text colors

--- frontend/src/components/cart/BillingConfirmation.js
+++ frontend/src/components/cart/BillingConfirmation.js
@@ -7,6 +7,7 @@ const useStyles = makeStyles(theme => ({
   heading: {
     color: theme.palette.secondary.main,
     fontSize: "1.5rem",
+    color: "959595",
   },
   values: {
     fontSize: "1.25rem",
